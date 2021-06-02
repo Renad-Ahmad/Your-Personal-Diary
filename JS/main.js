e041a031527e221fc8a5b8b1983a42af230f0d22
Renad-Ahmad <rena.34end@gmail.com>
2021-06-02 22:36:31 +0300

    fix style add js function to add new post

---
+++ JS/main.js
@@ -0,0 +1,23 @@
+
+// let form = document.getElementById("myForm").submit();
+//  let saveForm = document.getElementById("root").innerHTML=form;
+let posts =[];
+function submitFunction(){
+    let form = document.getElementById("my-form"); 
+    
+    for(let i = 0; i < form.elements.length; i++){
+       let e = form.elements[i];
+       posts.push(e.value);
+       console.log(e.value);
+
+       var btn = document.createElement("p");   
+       btn.innerHTML = e.value;                   
+       document.getElementById("root").appendChild(btn); 
+    }
+     console.log(posts);
+    
+}
+ 
+
+
+
