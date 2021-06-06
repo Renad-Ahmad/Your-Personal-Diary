ff17db94f4b705457377b4fbae1a2d98a044bec2
Renad-Ahmad <rena.34end@gmail.com>
2021-06-06 04:42:13 +0300

    Add a date to the post

--- JS/main.js
+++ JS/main.js
@@ -1,24 +1,22 @@
-
-// let form = document.getElementById("myForm").submit();
-//  let saveForm = document.getElementById("root").innerHTML=form;
-let posts =[];
+let posts = [];
 function submitFunction(){
     let form = document.getElementById("my-form"); 
-    
+    //const date = new Date().toTimeString().split(" ")[0];
+    const date = Date().slice(0,21);
+
+
     for(let i = 0; i < form.elements.length; i++){
        let e = form.elements[i];
+
         if(e.value != ""){
             posts.push(e.value);
-            console.log(e.value);
             var btn = document.createElement("p");   
-            btn.innerHTML = e.value;                   
+             btn.innerHTML = e.value + "\n" + date;                 
             document.getElementById("root").appendChild(btn); 
         }
     }
-     console.log(posts);
-    
 }
- 
+
 
 
 
