2c3b3a4c6a600fbea0ea2d39174b234b1c9377dc
Renad-Ahmad <rena.34end@gmail.com>
2021-06-06 09:03:17 +0300

    delete function it kind of works

--- JS/main.js
+++ JS/main.js
@@ -1,22 +1,36 @@
 let posts = [];
-function submitFunction(){
+
+function add(){
     let form = document.getElementById("my-form"); 
     //const date = new Date().toTimeString().split(" ")[0];
     const date = Date().slice(0,21);
 
-
+    
     for(let i = 0; i < form.elements.length; i++){
        let e = form.elements[i];
 
         if(e.value != ""){
             posts.push(e.value);
-            var btn = document.createElement("p");   
-             btn.innerHTML = e.value + "\n" + date;                 
-            document.getElementById("root").appendChild(btn); 
+
+            let paragraph = document.createElement("p");
+            paragraph.innerHTML = e.value + "\n" + date ;                 
+            document.getElementById("root").appendChild(paragraph);
+
+            let remove = document.createElement("INPUT");
+            remove.setAttribute("type", "button");
+            remove.setAttribute("value","DELETE")
+            remove.setAttribute("onclick","remove()")
+            document.getElementById("root").appendChild(remove); 
         }
     }
+    console.log(posts); 
 }
 
-
+function remove(){
+    posts.map((item, index) => {
+        console.log( item +" " + index )  
+        return posts.splice(index, 1);
+      })
+}
 
 
