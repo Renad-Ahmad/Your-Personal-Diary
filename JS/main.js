0677ae32d76d4b8de8f64f9b524a26ebcffeb450
Renad-Ahmad <rena.34end@gmail.com>
2021-06-11 20:59:40 +0300

    delete function works

--- JS/main.js
+++ JS/main.js
@@ -1,36 +1,49 @@
-let posts = [];
+const form = document.getElementById("my-form"); 
+const allPosts = document.getElementsByClassName("all-posts");
+const date = Date().slice(0,21);
 
 function add(){
-    let form = document.getElementById("my-form"); 
-    //const date = new Date().toTimeString().split(" ")[0];
-    const date = Date().slice(0,21);
-
-    
     for(let i = 0; i < form.elements.length; i++){
        let e = form.elements[i];
-
         if(e.value != ""){
-            posts.push(e.value);
 
+            let div = document.createElement("div");
             let paragraph = document.createElement("p");
-            paragraph.innerHTML = e.value + "\n" + date ;                 
-            document.getElementById("root").appendChild(paragraph);
-
             let remove = document.createElement("INPUT");
+
+            paragraph.innerHTML = e.value + "\n" + date ;                 
+            div.appendChild(paragraph);
+
             remove.setAttribute("type", "button");
             remove.setAttribute("value","DELETE")
-            remove.setAttribute("onclick","remove()")
-            document.getElementById("root").appendChild(remove); 
+            remove.setAttribute("onclick","remove(this)")
+            div.appendChild(remove); 
+            allPosts[i].appendChild(div);
+
         }
     }
-    console.log(posts); 
 }
 
-function remove(){
-    posts.map((item, index) => {
-        console.log( item +" " + index )  
-        return posts.splice(index, 1);
-      })
+// function moveLastArrayElementToFirstIndex(this_array) {
+//     let new_array = new Array();
+//     new_array[0] = this_array[this_array.length - 1];     
+//     for (i = 1; i < this_array.length; i++) { 
+//       new_array[i] = this_array[i - 1];
+//     }
+//     return new_array;
+//   }
+
+function remove(element){
+    element.parentElement.parentElement
+    .removeChild(element.parentElement);
+
 }
+
+// function edit(){
+//     posts.map((item, index) => {
+//         let test = posts.splice(index, 1,item);
+//         return test;
+//     })
+// }
 
 
